7bc5fcf5574a6336f8eb5e6cb7044d4ebdeba6b9
Philipp Kamps <pkamps@mugo.ca>
2014-05-14 16:17:05 +0200

    Disable user hash generation by default

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -6,7 +6,7 @@ StaticCacheHandler=StaticCacheMugoVarnish
 
 # Use this event listener if you would like to set the vuserhash cookie.
 # For more details see the example varnish config file in doc/examples/userhash.vcl
-[Event]
-Listeners[]=session/regenerate@MugoVarnishEvents::regenerateSession
+#[Event]
+#Listeners[]=session/regenerate@MugoVarnishEvents::regenerateSession
 
 */ ?>
