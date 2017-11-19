8cfe6451cd1c74e22e42de340e4cffcb30a03dcd
pkamps <pkamps@mugo.ca>
2017-11-19 10:39:20 +0200

    Adding a reference to the blog post

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -5,7 +5,10 @@ StaticCache=enabled
 StaticCacheHandler=StaticCacheMugoVarnish
 
 # Use these event listeners if you would like to set the vuserhash cookie.
-# For more details see the example varnish config file in doc/examples/userhash.vcl
+# For more details see
+# - the example varnish config file in doc/examples/userhash.vcl
+# - a blog post describing the feature
+# https://www.mugo.ca/Blog/Varnish-caching-of-non-sensitive-content-for-logged-in-users
 [Event]
 #Listeners[]=response/preoutput@MugoVarnishEvents::preoutput
 #Listeners[]=session/regenerate@MugoVarnishEvents::regenerateSession
