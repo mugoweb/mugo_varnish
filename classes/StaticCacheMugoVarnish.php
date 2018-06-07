34d811905ed7c60a2dcb2ff94797e911bb81638d
Luca Realdi <lr@opencontent.it>
2018-06-07 22:27:14 +0200

    Customizable PageUrlBuilder

--- classes/StaticCacheMugoVarnish.php
+++ classes/StaticCacheMugoVarnish.php
@@ -6,12 +6,11 @@
  */
 class StaticCacheMugoVarnish implements ezpStaticCache
 {
-    private $pathPrefixModifier = '';
-    private $uriTransformation = false;
-    private $urlModifierMatch = '';
-    private $urlModifierReplace = '';
-    private $omitUrlPatterns = array();
-    private $purgeSystemUrls = false;
+    /**
+     * @var MugoVarnishBuilderInterface
+     */
+    private $builder;
+
     protected $settings;
     
     static private $cleanUpHandlerRegistered = false;
@@ -20,33 +19,26 @@ class StaticCacheMugoVarnish implements ezpStaticCache
     public function __construct()
     {
         $this->settings = eZINI::instance( 'mugo_varnish.ini' );
-        
-        if( $this->settings->hasVariable( 'PurgeUrlBuilder', 'PathPrefixModifier' ) )
+
+        if( $this->settings->hasVariable( 'PurgeUrlBuilder', 'BuilderClass' ) )
         {
-            $this->pathPrefixModifier = $this->settings->variable( 'PurgeUrlBuilder', 'PathPrefixModifier' );
+            $builderClass = $this->settings->variable( 'PurgeUrlBuilder', 'BuilderClass' );
+            if (class_exists($builderClass))
+            {
+                $builder = new $builderClass();
+                if ($builder instanceof MugoVarnishBuilderInterface)
+                {
+                    $this->builder = $builder;
+                }
+            }
         }
 
-        if( $this->settings->hasVariable( 'PurgeUrlBuilder', 'UriTransformation' ) )
+        // set default builder class if missing settings
+        if ($this->builder === null)
         {
-            $this->uriTransformation = $this->settings->variable( 'PurgeUrlBuilder', 'UriTransformation' ) == 'enabled' ? true : false;
-        }
-        
-        if( $this->settings->hasVariable( 'PurgeUrlBuilder', 'ModifierMatch' ) )
-        {
-            $this->urlModifierMatch   = $this->settings->variable( 'PurgeUrlBuilder', 'ModifierMatch' );
-            $this->urlModifierReplace = $this->settings->variable( 'PurgeUrlBuilder', 'ModifierReplace' );
+            $this->builder = new MugoVarnishPageUrlBuilder();
         }
 
-        if( $this->settings->hasVariable( 'PurgeUrlBuilder', 'OmitUrlPatterns' ) )
-        {
-            $this->omitUrlPatterns = $this->settings->variable( 'PurgeUrlBuilder', 'OmitUrlPatterns' );
-        }
-
-        if( $this->settings->hasVariable( 'PurgeUrlBuilder', 'PurgeSystemURL' ) )
-        {
-            $this->purgeSystemUrls = $this->settings->variable( 'PurgeUrlBuilder', 'PurgeSystemURL' ) == 'enabled' ? true : false;
-        }
-        
         // Register Cleanup Hanlder to purge urls at the end of the request
         if( ! self::$cleanUpHandlerRegistered )
         {
@@ -72,39 +64,46 @@ class StaticCacheMugoVarnish implements ezpStaticCache
             {
                 self::$banConditions = array_merge(
                     self::$banConditions,
-                    $this->urls2BanConditions( $this->nodeId2Urls( $nodeId ) )
+                    $this->builder->buildConditionForNodeIdCache( $nodeId )
                 );
             }
         }
         
-        eZDebug::accumulatorStop( 'StaticCacheMugo', '', 'StaticCacheMugo' );
+        eZDebug::accumulatorStop( 'StaticCacheMugo', '' );
     }
     
     
     /**
      * Purges all varnish cache
-     * 
+     *
+     * @param bool $force If true then it will create all static caches even if it is not outdated.
+     * @param bool $quiet If true then the function will not output anything.
+     * @param eZCLI|false $cli The eZCLI object or false if no output can be done.
+     * @param bool $delay
+     *
+     * @return bool
      */
     public function generateCache( $force = false, $quiet = false, $cli = false, $delay = true )
     {
         if( $force )
         {
-            $purger = VarnishPurger::Instance();
-            return $purger->purge( '.*', true );
+            self::$banConditions[] = $this->builder->buildConditionForAllCache();
         }
         else
         {
             // Not sure if that function ever gets called without force
         }
+
+        return false;
     }
     
     /**
      * Make interface happy
-     * 
-     * @param type $url
-     * @param type $nodeID
-     * @param type $skipExisting
-     * @param type $delay
+     *
+     * @param string $url The URL to cache, e.g /news
+     * @param int|false $nodeID The ID of the node to cache, if supplied it will also cache content/view/full/xxx.
+     * @param bool $skipExisting If true it will not unlink existing cache files.
+     * @param bool $delay
      */
     public function cacheURL( $url, $nodeID = false, $skipExisting = false, $delay = true )
     {}
@@ -112,55 +111,10 @@ class StaticCacheMugoVarnish implements ezpStaticCache
     /**
      * Make interface happy
      * 
-     * @param type $url
+     * @param string $url The URL for the current item, e.g /news
      */
     public function removeURL( $url )
     {}
-
-    /**
-     * @param integer $urls
-     * @return array
-     */
-    protected function urls2BanConditions( $urls )
-    {
-        $return = array();
-
-        if( !empty( $urls ) )
-        {
-            foreach( $urls as $url )
-            {
-                $condition = 'obj.http.X-Ban-Url ~ ^'. $url .'(/?\(.*)?$';
-
-                if( $this->settings->hasVariable( 'PurgeUrlBuilder', 'HostMatching' ) )
-                {
-                    switch( $this->settings->variable( 'PurgeUrlBuilder', 'HostMatching' ) )
-                    {
-                        case 'SiteURL':
-                        {
-                            $ini = eZINI::instance();
-                            $siteUrl = $ini->variable( 'SiteSettings', 'SiteURL' );
-
-                            if( $siteUrl )
-                            {
-                                $condition .= ' && obj.http.X-Ban-Host ~ ' . preg_quote( $siteUrl );
-                            }
-                        }
-                        break;
-
-                        case 'RelatedSiteAccessList':
-                        {
-
-                        }
-                        break;
-                    }
-                }
-
-                $return[] = $condition;
-            }
-        }
-
-        return $return;
-    }
 
     /**
      * Generates a user hash for the current user.
@@ -180,87 +134,12 @@ class StaticCacheMugoVarnish implements ezpStaticCache
         }
         return md5( serialize( $hashArray ) );
     }
-
-    /**
-     * @param string $nodeId
-     * @return array
-     */
-    protected function nodeId2Urls( $nodeId )
-    {
-        $elements = eZURLAliasML::fetchByAction( 'eznode', $nodeId, false, false, true );
-
-        $urls = array();
-        foreach( $elements as $element )
-        {
-            $urls[] = '/' . $element->getPath();
-        }
-        
-        if( $this->purgeSystemUrls )
-        {
-            $urls[] = '/content/view/full/' . $nodeId;
-        }
-
-        $transformedUrls = $this->transformUrls( $urls );
-
-        return $transformedUrls;
-    }
-
-    /**
-     * @param array $urls
-     * @return array
-     */
-    protected function transformUrls( $urls )
-    {
-        foreach( $urls as $i => $url )
-        {
-            if( $this->pathPrefixModifier )
-            {
-                $this->applyPreFixModifier( $url );
-            }
-
-            if( $this->uriTransformation )
-            {
-                eZURI::transformURI( $url );
-            }
-
-            if( $this->urlModifierMatch )
-            {
-                $url = preg_replace( $this->urlModifierMatch, $this->urlModifierReplace, $url );
-            }
-
-            // exclude URLs based on configured patterns - untested
-            if( !empty( $this->omitUrlPatterns ) )
-            {
-                foreach( $this->omitUrlPatterns as $pattern )
-                {
-                    if( preg_match( $pattern, $url ) )
-                    {
-                        // skip it and process next URL
-                        unset( $urls[ $i ] );
-                        continue 2;
-                    }
-                }
-            }
-
-            $urls[ $i ] = $url;
-        }
-
-        return $urls;
-    }
-
-    protected function applyPreFixModifier( &$url )
-    {
-        $reg = '/^' . preg_quote( $this->pathPrefixModifier, '/' ) . '/';
-        $url = preg_replace( $reg, '', $url );
-    }
     
     static function executeActions()
     {}
-    
-    /**
-     *
-     */
-    static function purgeList()
+
+
+    public static function purgeList()
     {
         $ini_varnish = eZINI::instance( 'mugo_varnish.ini' );
         $maxBanRequests = $ini_varnish->variable( 'VarnishSettings', 'MaxBanRequests' );
@@ -289,8 +168,67 @@ class StaticCacheMugoVarnish implements ezpStaticCache
         {
             foreach( $banConditionsLimited as $condition )
             {
-                $purger->purge( $condition, true );
+                $purger->purge( $condition );
             }
         }
     }
+
+    /**
+     * Only for backward compatibility
+     *
+     * @deprecated
+     *
+     * @param integer $urls
+     * @return array
+     */
+    protected function urls2BanConditions( $urls )
+    {
+        $builder = new MugoVarnishPageUrlBuilder();
+
+        return $builder->urls2BanConditions( $urls );
+    }
+
+    /**
+     * Only for backward compatibility
+     *
+     * @deprecated
+     *
+     * @param string $nodeId
+     * @return array
+     */
+    protected function nodeId2Urls( $nodeId )
+    {
+        $builder = new MugoVarnishPageUrlBuilder();
+
+        return $builder->nodeId2Urls( $nodeId );
+    }
+
+    /**
+     * Only for backward compatibility
+     *
+     * @deprecated
+     *
+     * @param array $urls
+     * @return array
+     */
+    protected function transformUrls( $urls )
+    {
+        $builder = new MugoVarnishPageUrlBuilder();
+
+        return $builder->transformUrls( $urls );
+    }
+
+    /**
+     * Only for backward compatibility
+     *
+     * @deprecated
+     *
+     * @param $url
+     */
+    protected function applyPreFixModifier( &$url )
+    {
+        $builder = new MugoVarnishPageUrlBuilder();
+        $builder->applyPreFixModifier( $url );
+    }
 }
+
